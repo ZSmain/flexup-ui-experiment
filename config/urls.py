3a6a641bbe99d6c3621c71f538cbf4d55c6096ad
Ismail <ishmael.zouaoui@gmail.com>
2025-01-16 18:13:30 +0100

    Add Django Debug Toolbar extension to the project.

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    
+    urlpatterns += debug_toolbar_urls()
